19d9374ceddf21c75a9cfd2efdbe9c6bc0ed53d2
Trung Do <67167385+Antonn114@users.noreply.github.com>
2024-05-08 08:54:46 +0700

    beta 1.0

--- app.js
+++ app.js
@@ -29,11 +29,17 @@ $(function(){
   });
 
   var $selectedNode;
-  
-  var $nodeAction_pending = false;
+
+  var $nodeAction = null;
+  var $nodeActionTarget = null;
 
 
   $('.orgchart').addClass('noncollapsable'); // deactivate
+
+  $('#treeview-node-field').hide();
+  $('#treeview-node-field-submit').hide();
+  $('#treeview-delete-node').hide();
+  $('#treeview-add-child').hide();
 
   $('#treeview-reset').on('click', function(event){
     $('.orgchart').css('transform','');
@@ -45,17 +51,24 @@ $(function(){
 
   oc.$chartContainer.on('click', '.node', function() {
     $selectedNode = $(this);
+    $('#treeview-delete-node').show();
+    $('#treeview-add-child').show();
   });
 
   oc.$chartContainer.on('click', '.orgchart', function(event) {
     if (!$(event.target).closest('.node').length) {
       $selectedNode = null;
+      $('#treeview-delete-node').hide();
+      $('#treeview-add-child').hide();
     }
   });
 
 
 
   $('#treeview-delete-node').on('click', function() {
+    if ($nodeAction){
+      return;
+    }
     var $node = $selectedNode;
     if (!$node) {
       alert("No node selected! Please try again.");
@@ -79,25 +92,32 @@ $(function(){
     });
 
   });
-  var $addChild_Text = null;
-  var 
+
+
   $('#treeview-add-child').on('click', function() {
     var $node = $selectedNode;
     if (!$node) {
       alert("No node selected! Please try again.");
       return;
     }
-    if ($nodeAction_pending){
+    if ($nodeAction){
       return;
     }
-    $nodeAction_pending = true;
-    $('#treeview-add-child-text').show();
+    $nodeAction = "add";
+    $nodeActionTarget = $node;
+    $node.toggleClass("node-action-target");
+    $selectedNode = null;
+    $('#treeview-node-field').show();
+    $('#treeview-node-field-submit').show();
+    $('#treeview-node-field').focus();
+    $node.toggleClass("focused");
+
   });
-  $(document).on('keypress',function(e) {
-    if(e.which == 13 && $addChild_pending) {
-      var $node = $selectedNode;
+  $('#treeview-node-field-submit').on('click', function(){
+    if($nodeAction == "add") {
+      var $node = $nodeActionTarget;
       if (!$node) {
-        alert("No node selected! Please try again.");
+        alert("No action target! Please report this issue to the creator.");
         return;
       }
       var $addChild_Text = null;
@@ -106,20 +126,27 @@ $(function(){
       }else{
         alert("Input field is empty! Please try again.");
       }
+      newid = getId();
       if (!$node.siblings('.nodes').length) {
-        oc.addChildren($node, [{'title': $addChild_Text, 'relationship': '110', 'id': getId() }]);
+        oc.addChildren($node, [{'title': $addChild_Text, 'relationship': '110', 'id': newid }]);
         // Be careful! Orgchart doesn't sort a node's children.
         // If the intended array of children is something like [1, 2, 3, 4, 5]
         // Then Orgchart makes it [2, 1, 3, 4, 5]
         // I have already had various ideas to fix this, but since this is a prototype,
         // My attention lies elsewhere!
       }else{
-        oc.addSiblings($node.siblings('.nodes').find('.node:first'), [{ 'title': $addChild_Text, 'relationship': '110', 'id': getId() }]);
+        oc.addSiblings($node.siblings('.nodes').find('.node:first'), [{ 'title': $addChild_Text, 'relationship': '110', 'id': newid }]);
       }
       $('#treeview-node-field').val("");
       $('#treeview-node-field').hide();
+      $('#treeview-node-field-submit').hide();
+      $(`#${newid}`).focus();
+      $(`#${newid}`).toggleClass("focused");
+      $selectedNode = $(`#${newid}`);
 
-      $addChild_pending = false;
+      $nodeAction = null;
+      $nodeActionTarget = null;
+      $node.toggleClass("node-action-target");
     }
     return;
   });
